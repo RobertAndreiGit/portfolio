6071f556227f2dce3b91c52249904fdecc664995
Amarandei Robert-Andrei <robert_amarandei@yahoo.com>
2021-03-02 15:43:32 +0200

    Update responsiveness

--- src/components/Information/InformationCard.js
+++ src/components/Information/InformationCard.js
@@ -4,7 +4,11 @@ export default function InformationCard({ data, alignRight }) {
   return (
     <div className="list-item">
       <div className="list-icon"></div>
-      <div className={`list-content ${alignRight && "float-right text-left"}`}>
+      <div
+        className={`list-content ${
+          alignRight && "lg:float-right lg:text-left"
+        }`}
+      >
         <p className="inline-block bg-black bg-opacity-10 p-2.5 rounded">{`${data.start} - ${data.end}`}</p>
         <p className="font-title text-3xl uppercase my-2">{data.title}</p>
         <p className="text-xl opacity-90">{data.subtitle}</p>
